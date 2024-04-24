b8df81e371076c95fbbdfb0dc5830e03e7d17a98
gabedurojas <91133222+gabedurojas@users.noreply.github.com>
2024-04-24 17:27:08 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,3 +4,14 @@ const navMenu = document.querySelector(".nav-menu")
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
 });
+
+// funcionalidad de desplazar hacia arriba
+const desplazarArriba = document.querySelector("#desplazarse-hacia-arriba");
+
+desplazarArriba.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
