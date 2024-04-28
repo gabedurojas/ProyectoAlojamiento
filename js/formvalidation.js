2038b9a1d05e7ee869b8ad22082cac9116242881
SilvanaMFarias <silvanamarcela.farias@estudiantes.unahur.edu.ar>
2024-04-28 00:45:11 -0300

    Modificaciones en formulario de contacto (Aspecto y validaçión js

--- js/formvalidation.js
+++ js/formvalidation.js
@@ -1,18 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const botonEnviar = document.querySelector("#formulario button[type='submit']");
+function validateForm() {
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value.trim();
+    var telefono = document.getElementById("telefono").value.trim();
+    var mensaje = document.getElementById("mensaje").value;
+    var motivo = document.getElementById("motivo").value;
+    var contactoEmail = document.getElementById("contactoEmail").checked;
+    var contactoTelefono = document.getElementById("contactoTelefono").checked;
+    var error = "";
 
-    botonEnviar.addEventListener("click", function(event) {
-        event.preventDefault();
+    //Validación de motivo
+    if (motivo == "") {
+        error += "Por favor, selecciona un motivo de contacto.\n";
+    }
 
-        const nombre = document.getElementById("nombre").value.trim();
-        const email = document.getElementById("email").value.trim();
-        const mensaje = document.getElementById("mensaje").value.trim();
+    //Validación check box contacto
+    if (!contactoEmail && !contactoTelefono) {
+        error += "Por favor, selecciona al menos una forma de contacto.\n";
+    }
 
-        if (nombre === "" || email === "" || mensaje === "") {
-            alert("Por favor, completa todos los campos.");
-        } else {
-            alert("Formulario válido. Datos enviados.");
-            // Aquí puedes agregar la lógica para enviar los datos a un servidor si lo necesitas
-        }
-    });
-});
+    if (error != "") {
+        alert(error);
+        return false;
+    }
+    
+    alert("Formulario válido. Datos enviados.")
+    return true;
+}
